78175924663a83dba33c783c1399d3e26a579a95
ferrevdd <ferre.vandendriessche@gmail.com>
2022-06-01 18:21:21 +0200

    Create inutils.py

---
+++ inutils.py
@@ -0,0 +1,64 @@
+class datasets():
+    LookUpOpts = ['ip',
+                    'region',
+                    'city',
+                    'organisation',
+                    'rawlocation',
+                    'postalcode',
+                    'timezone',
+                    'anycaststatus',
+                    'country']
+    
+    IpInfoLookupVals =['ip',
+                   'region',
+                   'city',
+                   'org',
+                   'loc',
+                   'postal',
+                   'timezone',
+                   'anycast',
+                   'country']
+class ip():
+    
+    def get():
+        import requests
+        ip = requests.get('https://api.ipify.org/').text
+        return ip
+    
+    
+    def lookup(ip,DataType=''):
+        import requests
+        data = requests.get(f'https://ipinfo.io/{ip}/json').json()
+        data.pop('readme')
+          
+        if len(DataType) == 0:
+            return data
+        else:
+            for i in range(len(datasets.LookUpOpts)):
+                if DataType == datasets.LookUpOpts[i]:
+                    return data[datasets.IpInfoLookupVals[i]]
+                
+    def ping(ip):
+        import subprocess;from subprocess import DEVNULL
+        return True if (subprocess.call(f'ping -n 1 {ip}',stdout=DEVNULL, stderr=subprocess.STDOUT) == 0) else False
+    
+    def HostToIp(host):
+        
+        import socket
+        return socket.gethostbyname(host)
+    
+    def CheckPort(ip, port:int, tout =0.5):
+        import socket
+        from socket import timeout
+        socket.setdefaulttimeout(tout)
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        result = s.connect_ex((ip,port))
+        s.close()
+        return True if result ==0 else False
+            
+
+print('This file should not be run as a standalone script\nPlease import this file as \n"from inutils import ip"\nto use the functions of this file')if (__name__ =='__main__')else ('')
+            
+        
+            
+    
