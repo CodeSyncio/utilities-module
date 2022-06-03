abb762e0173808996cc5b8a2b7c0a34ffcb19f74
ferrevdd <ferre.vandendriessche@gmail.com>
2022-06-03 20:47:08 +0200

    more base... options

--- inutils.py
+++ inutils.py
@@ -1,27 +1,27 @@
-from hashlib import sha256
-from requests import request
-
-
 class datasets():
-    LookUpOpts = ['ip',
-                    'region',
-                    'city',
-                    'organisation',
-                    'rawlocation',
-                    'postalcode',
-                    'timezone',
-                    'anycaststatus',
-                    'country']
     
-    IpInfoLookupVals =['ip',
-                   'region',
-                   'city',
-                   'org',
-                   'loc',
-                   'postal',
-                   'timezone',
-                   'anycast',
-                   'country']
+    class IpInfoIdentifiers():
+    
+    
+        LookUpOpts =    ['ip',
+                        'region',
+                        'city',
+                        'organisation',
+                        'rawlocation',
+                        'postalcode',
+                        'timezone',
+                        'anycaststatus',
+                        'country']
+    
+        IpInfoLookupVals =['ip',
+                       'region',
+                       'city',
+                       'org',
+                       'loc',
+                       'postal',
+                       'timezone',
+                       'anycast',
+                       'country']
 class ip():
     
     def get():
@@ -38,9 +38,9 @@ class ip():
         if len(DataType) == 0:
             return data
         else:
-            for i in range(len(datasets.LookUpOpts)):
-                if DataType == datasets.LookUpOpts[i]:
-                    return data[datasets.IpInfoLookupVals[i]]
+            for i in range(len(datasets.IpInfoIdentifiers.LookUpOpts)):
+                if DataType == datasets.IpInfoIdentifiers.LookUpOpts[i]:
+                    return data[datasets.IpInfoIdentifiers.IpInfoLookupVals[i]]
                 
     def ping(ip):
         import subprocess;from subprocess import DEVNULL
@@ -62,17 +62,47 @@ class ip():
     
 class crypt():
     
-    class base64():
+    class base():
         
-        def encode(txtstring:str):
+        class GetType():
+            
+            def encinputter(inp):
+                import base64
+                if inp == 64:
+                    return base64.b64encode
+                elif inp == 32:
+                    return base64.b32encode
+                elif inp == 16:
+                    return base64.b16encode
+                elif inp == 85:
+                    return base64.b85encode
+                
+            def deencrypto(inp):
+                import base64
+                if inp == 64:
+                    return base64.b64decode
+                elif inp == 32:
+                    return base64.b32decode
+                elif inp == 16:
+                    return base64.b16decode
+                elif inp == 85:
+                    return base64.b85decode
+    
+        def encode(txtstring:str,encinput:int):
+            
             import base64,cryptography
-            base = base64.b64encode(txtstring.encode('utf-8'))
+            base = crypt.base.GetType.encinputter(encinput)(txtstring.encode('utf-8'))
             return str(base, "utf-8")
             
-        def decode(txtstring:str):
+        def decode(txtstring:str,encinput:int):
             import base64
-            base = base64.b64decode(txtstring.encode('utf-8'))
+            base = crypt.base.GetType.deencrypto(encinput)(txtstring.encode('utf-8'))
             return str(base, "utf-8")
+        
+        
+    
+        
+    
             
 
 print('This file should not be run as a standalone script\nPlease import this file as \n"from inutils import ip"\nto use the functions of this file')if (__name__ =='__main__')else ('')
---
+++ test.py
@@ -0,0 +1,3 @@
+from inutils import crypt
+
+print ( crypt.base.encode('hello',16) )
